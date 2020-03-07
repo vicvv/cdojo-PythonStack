1ed5a9b93cc64ad929087166735e5a3073514e1d
vicvv <xolosno270@gmail.com>
2020-03-07 14:37:35 -0800

    all pyc files removed

--- python/fundamentals/bubblesort.py
+++ python/fundamentals/bubblesort.py
@@ -8,11 +8,11 @@ def myBblsort(array):
     for j in range(len(array)- 1):
         for i in range(len(array)- 1 - j):
             count += 1
-            print("*" * 80 )
+            print("-" * 80 )
             if array[i] > array[i+1]:
-                print("index:", i, "swapping", array[i], "and", array[i+1])
+                #print("index:", i, "swapping", array[i], "and", array[i+1])
                 array[i], array[i+1] =  array[i+1],array[i]
-                print("new array:", array)
+                #print("new array:", array)
             else:
                 print("no need to swap", array[i], "and", array[i+1])
     print("number of evaluations: ", count)
